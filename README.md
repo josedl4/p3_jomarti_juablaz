8be62cf56136445ba489ce926e0cb03c503c0ea4
jomarti <joseluis.martin.martin@alumnos.uva.es>
2016-12-11 12:50:14 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+p3_jomarti_juablaz
---
+++ src/main/java/uva/equipo02/p3_jomarti_juablaz/App.java
@@ -0,0 +1,13 @@
+package uva.equipo02.p3_jomarti_juablaz;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
